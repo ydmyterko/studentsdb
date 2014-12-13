c891f216491fc172ee8bd84c75965e1e635fc201
Yuriy Dmyterko <yuriy.dmyterko@gmail.com>
2014-12-13 23:42:51 +0200

    add templates folder

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -2,9 +2,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'studentsdb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+  # Students urls
+  url(r'^$', 'students.views.students_list', name='home'),
+  url(r'^students/add/$', 'students.views.students_add', name='students_add'),
+  url(r'^students/(?P<sid>\d+)/edit/$', 'students.views.students_edit', name='students_edit'),
+  url(r'^students/(?P<sid>\d+)/delete/$', 'students.views.students_delete', name='students_delete'),
 
-    url(r'^admin/', include(admin.site.urls)),
+  # Groups urls
+  url(r'^groups/$', 'students.views.groups_list', name='groups'),
+  url(r'^groups/add/$', 'students.views.groups_add', name='groups_add'),
+  url(r'^groups/(?P<gid>\d+)/edit/$', 'students.views.groups_edit', name='groups_edit'),
+  url(r'^groups/(?P<gid>\d+)/delete/$', 'students.views.groups_delete', name='groups_delete'),
+  url(r'^admin/', include(admin.site.urls)),
 )
