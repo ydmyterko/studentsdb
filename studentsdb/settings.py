e74d435dfab28767cf5fad7347919df898580086
Yuriy Dmyterko <yuriy.dmyterko@gmail.com>
2014-12-20 23:29:35 +0200

    view reorganazed

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -8,10 +8,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+from django.conf import global_settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+PORTAL_URL = 'http://localhost:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -82,3 +85,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_CONTEXT_PROCESSORS = \
+   global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+   "django.core.context_processors.request",
+   "studentsdb.context_processors.students_proc",
+   )
